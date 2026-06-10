18b12944b7864f07bca6772a47eb1bc6ff82e0ed
Gabriel Linhares <gabriellr2301@outlook.com>
2026-06-09 23:00:58 -0300

    terraform_fmt

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,21 +1,21 @@
 resource "aws_dynamodb_table" "saas_project_expenses" {
-    name         = var.expenses_table
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "PK"
-    range_key = "SK"
+  name         = var.expenses_table
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "PK"
+  range_key    = "SK"
 
-    tags = {
+  tags = {
     Environment = var.tag_Environment
     Project     = var.tag_Project
-    }
+  }
 
-    attribute {
+  attribute {
     name = "PK"
     type = "S"
-    }
+  }
 
-    attribute {
+  attribute {
     name = "SK"
     type = "S"
-    }
+  }
 }
